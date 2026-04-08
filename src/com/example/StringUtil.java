a08595ccb8d4e38ca7cee8c13d99c0e47d034af8
NickBezborodov <benlada1488@mail.ru>
2026-04-08 16:52:30 +0300

    Убрал, добавил отступы где нужно

--- src/com/example/StringUtil.java
+++ src/com/example/StringUtil.java
@@ -1,26 +1,27 @@
 package com.example;
 
 public class StringUtil {
-    private StringUtil() {}
-        public static String reverseOnlyLetters (String str){
-            char[] ch = str.toCharArray();
 
-            for (int left = 0, right = str.length() - 1; left < right; ) {
-
-                if (!Character.isLetter(ch[left])) {
-                    left++;
-                } else if (!Character.isLetter(ch[right])) {
-                    right--;
-
-
-                } else {
-                    char temp = ch[left];
-                    ch[left] = ch[right];
-                    ch[right] = temp;
-                    left++;
-                    right--;
-                }
-            }
-            return new String(ch);
-        }
+    private StringUtil() {
     }
+
+    public static String reverseOnlyLetters(String str) {
+        char[] ch = str.toCharArray();
+
+        for (int left = 0, right = str.length() - 1; left < right; ) {
+            if (!Character.isLetter(ch[left])) {
+                left++;
+            } else if (!Character.isLetter(ch[right])) {
+                right--;
+            } else {
+                char temp = ch[left];
+                ch[left] = ch[right];
+                ch[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+        return new String(ch);
+    }
+}
